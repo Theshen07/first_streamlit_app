32c3af8b02282f67c800aea3e656605e2e233026
Theshen <110827105+Theshen07@users.noreply.github.com>
2022-08-08 14:25:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text(:bowl_with_spoon:'Omega 3 & Blueberry oatmeal')
 streamlit.text(:green_salad:'Kale, Spinach & Rocket Smoothie')
-streamlit.text(:chicken: 'Hard-Boiled Free-Range Egg')
+streamlit.text(chicken 'Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
