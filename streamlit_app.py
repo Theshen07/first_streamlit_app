2d9b0976834d92eb5ff9f756bb3ab68f213cd23b
Theshen <110827105+Theshen07@users.noreply.github.com>
2022-08-08 14:24:09 +0200

    Update streamlit_app.py
    Updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kalse, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry oatmeal')
+streamlit.text(':green_salad:Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
