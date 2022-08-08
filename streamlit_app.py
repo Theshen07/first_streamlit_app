aab4b8d4f1f4dc30916bfa66438cf29814a4f636
Theshen <110827105+Theshen07@users.noreply.github.com>
2022-08-08 14:24:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry oatmeal')
-streamlit.text(':green_salad:Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text(:bowl_with_spoon:'Omega 3 & Blueberry oatmeal')
+streamlit.text(:green_salad:'Kale, Spinach & Rocket Smoothie')
+streamlit.text(:chicken: 'Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
